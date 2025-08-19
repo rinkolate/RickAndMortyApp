9eede9b13f56e9ce161db1859a29ff5ece67d7e8
Ekaterina Mitina <e.mitina@vkteam.ru>
2025-08-19 07:47:34 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Mitina Ekaterina on 19.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
